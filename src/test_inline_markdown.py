710fe5370051edff5dc914d2c11ab93368688f73
raibarra04 <ribarra764@gmail.com>
2025-06-11 23:57:53 -0700

    added inline tests

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,6 +1,6 @@
 import unittest
 
-from inline_markdown import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
+from inline_markdown import split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_images, split_nodes_links
 from textnode import TextNode, TextType
 
 class TestInlineMarkdown(unittest.TestCase):
@@ -133,6 +133,17 @@ class TestInlineMarkdown(unittest.TestCase):
         self.assertListEqual([
             ("to boot dev", "https://www.boot.dev"),
             ("to youtube", "https://www.youtube.com/@bootdotdev")], matches)
+    
+    def test_split_images(self):
+        node = TextNode(
+                "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)", TextType.TEXT,)
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual([
+            TextNode("This is text with an ", TextType.TEXT),
+            TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+            TextNode(" and another ", TextType.TEXT),
+            TextNode("second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"),],
+                             new_nodes,)
 
 
 if __name__ == "__main__":
